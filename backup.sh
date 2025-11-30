b6145fb0c2848dd63f03362730ed0b787304fdcd
zachakaquack <zach12399@gmail.com>
2025-11-30 08:59:47 -0600

    update 2025-11-30T14:59:47UTC

--- backup.sh
+++ backup.sh
@@ -4,18 +4,18 @@
 cd $HOME/dotfiles/
 
 # refresh the package list
-id=$(notify-send -p "updating pacman list (pkglist.txt)")
+id=$(notify-send -p "Updating AUR list...")
 pacman -Qqe > $HOME/dotfiles/pkglist.txt
 
-notify-send -r $id "updating pacman list (pkglist.txt)"
+notify-send -r $id "Updating YAY list..."
 yay -Qqe > $HOME/dotfiles/aurlist.txt
 
 # do git stuff
 
-notify-send -r $id "adding"
+notify-send -r $id "Adding files..."
 git add . > /dev/null 2>&1
 
-notify-send -r $id "updating"
+notify-send -r $id "Committing..."
 git commit -m "update $(date -u +%Y-%m-%dT%H:%M:%S%Z)" || true
 git push > /dev/null 2>&1
 notify-send -t 3000 -r $id "Done backing up dotfiles!"
