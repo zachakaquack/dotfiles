4f56f4fb4f17db3523d539412e5f8ab854567196
zachakaquack <zach12399@gmail.com>
2025-11-30 09:06:12 -0600

    update 2025-11-30T15:06:12UTC

--- backup.sh
+++ backup.sh
@@ -15,7 +15,9 @@ yay -Qqe > $HOME/dotfiles/aurlist.txt
 notify-send -r $id "Adding files..."
 git add . > /dev/null 2>&1
 
-notify-send -r $id "Committing..."
+# overwrite id and then wait forever
+sec=60
+notify-send -r $id -t $(($sec*60)) "Committing..."
 git commit -m "update $(date -u +%Y-%m-%dT%H:%M:%S%Z)" || true
 git push > /dev/null 2>&1
 notify-send -t 3000 -r $id "Done backing up dotfiles!"
