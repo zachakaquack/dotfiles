e06d617a665851ea14ea8ded6dd9818aa59945d8
zachakaquack <zach12399@gmail.com>
2025-11-30 08:59:21 -0600

    update 2025-11-30T14:59:21UTC

--- backup.sh
+++ backup.sh
@@ -7,17 +7,14 @@ cd $HOME/dotfiles/
 id=$(notify-send -p "updating pacman list (pkglist.txt)")
 pacman -Qqe > $HOME/dotfiles/pkglist.txt
 
-sleep 0.1
 notify-send -r $id "updating pacman list (pkglist.txt)"
 yay -Qqe > $HOME/dotfiles/aurlist.txt
 
 # do git stuff
 
-sleep 0.1
 notify-send -r $id "adding"
 git add . > /dev/null 2>&1
 
-sleep 0.1
 notify-send -r $id "updating"
 git commit -m "update $(date -u +%Y-%m-%dT%H:%M:%S%Z)" || true
 git push > /dev/null 2>&1
