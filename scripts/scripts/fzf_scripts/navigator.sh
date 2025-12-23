0bba072ea081dc96190ed7feea327cc1f76199fc
zachakaquack <zach12399@gmail.com>
2025-12-23 12:01:03 -0600

    update 2025-12-23T18:01:03UTC

--- scripts/scripts/fzf_scripts/navigator.sh
+++ scripts/scripts/fzf_scripts/navigator.sh
@@ -11,16 +11,17 @@ while true; do
     path=$(readlink -m "$cwd/$item")
 
     if [[ -d "$path" ]]; then
-        # directory
         cwd=$path
-    else
-        mime_type=$(file -b --mime-type "$path")
-
-        if [[ "$mime_type" == text/* ]]; then
-            kitty --directory "$cwd" sh -c "cat $HOME/.cache/wal/sequences; nvim \"$path\"; exec \$SHELL" &
-        else
-            xdg-open "$path" >/dev/null 2>&1 &
-        fi
-        exit 0
+        continue
     fi
+
+    mime_type=$(file -b --mime-type "$path")
+
+    if [[ "$mime_type" == text/* ]]; then
+        kitty --directory "$cwd" sh -c "cat $HOME/.cache/wal/sequences; nvim \"$path\"; exec \$SHELL" &
+    else
+        xdg-open "$path" >/dev/null 2>&1 &
+    fi
+    exit 0
+
 done
