5b9006be9f9b978e776fb2690e24bf135d0924c6
zachakaquack <zach12399@gmail.com>
2026-01-10 09:22:41 -0600

    update 2026-01-10T15:22:41UTC

--- scripts/scripts/fzf_scripts/launcher.sh
+++ scripts/scripts/fzf_scripts/launcher.sh
@@ -19,22 +19,10 @@ projects(){
     $SCRIPTS/fzf_scripts/find_projects.sh "$1"
 }
 
-twitter(){
-    gtk-launch WebApp-twitter3005.desktop
-}
-
 get_date(){
     d=$(date)
     wl-copy $d
     notify-send "Copied $d"
-}
-
-run_dysk(){
-    foot --app-id "fzf_dysk" sh -c "cat ~/.cache/wal/sequences; dysk; echo; read -n 1 _" &
-}
-
-run_speedtest(){
-    foot --app-id "fzf_speedtest" sh -c "cat ~/.cache/wal/sequences; speedtest++; echo; read -n 1 _" &
 }
 
 run_minecraft(){
@@ -43,8 +31,6 @@ run_minecraft(){
 
 random_theme(){
     $SCRIPTS/random.sh
-    # image=$(find $HOME/Pictures/bgs | shuf -n 1)
-    # $SCRIPTS/wal-tile.sh $image
 }
 
 
@@ -54,16 +40,9 @@ options+="Backup Dotfiles""\\n"
 options+="Search Python Projects""\\n"
 options+="Search C++ Projects (CPP)""\\n"
 options+="Search Repos""\\n"
-# options+="Twitter""\\n"
-# options+="Dysk (Disk)""\\n"
-# options+="SpeedTest""\\n"
 options+="Minecraft (MCSR)""\\n"
-# options+="Mission Center (Task Manager)""\\n"
 options+="Random Theme""\\n"
-options+="Attach Zellij Session""\\n"
-options+="New Zellij Session""\\n"
 options+="Define Word""\\n"
-# options+="Navigate / Open File""\\n"
 
 chooseprogram() { \
         d=$(date)
@@ -75,17 +54,10 @@ chooseprogram() { \
         "Search Python Projects") projects "python" ;;
         "Search C++ Projects (CPP)") projects "c++" ;;
         "Search Repos") projects "repos" ;;
-            # "Twitter") twitter ;;
-            # "Dysk (Disk)") run_dysk ;;
         $d) get_date ;;
-            # "SpeedTest") run_speedtest ;;
         "Minecraft (MCSR)") run_minecraft ;;
-            # "Mission Center (Task Manager)") missioncenter & ;;
         "Random Theme") random_theme ;;
-        "Attach Zellij Session") $SCRIPTS/fzf_scripts/zellij_attach.sh ;;
-        "New Zellij Session") $SCRIPTS/fzf_scripts/zellij_new_session.sh ;;
         "Define Word") $SCRIPTS/fzf_scripts/define_menu.sh ;;
-            # "Navigate / Open File") $SCRIPTS/fzf_scripts/navigator.sh ;;
     esac
 }
 
